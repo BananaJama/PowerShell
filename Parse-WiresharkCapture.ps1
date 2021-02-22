c374bf15e083d401e6d203f6b722e3f1eb828174
Jeremy Brown <Jeremy.Brown@nccourts.org>
2021-02-22 10:01:46 -0500

    Add Parse-Wireshak script

---
+++ Parse-WiresharkCapture.ps1
@@ -0,0 +1,25 @@
+param (
+    [Parameter(Mandatory)]
+    [string]
+    $FolderPath
+)
+
+$Files = Get-ChildItem $FolderPath -Include '*.pcap'
+
+foreach ($i in $Files) {
+    $FileName = $i.BaseName
+    $FullPath = $i.FullName
+    $Directory = $i.Directory
+
+    Write-Host "Processing $FullPath..."
+
+    $Splat = @{
+        FilePath = 'C:\Program Files\Wireshark\tshark.exe'
+        ArgumentList = "-T json -r $FullPath"
+        NoNewWindow = $true
+        Wait = $true
+        RedirectStandardOutput = "$Directory\$FileName.json"
+    }
+    Start-Process @Splat 
+
+}
