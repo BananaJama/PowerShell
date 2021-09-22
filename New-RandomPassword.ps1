2d05689353c0fb243dc7274508de0ebc3b6540c6
Jeremy Brown <Jeremy.Brown@nccourts.org>
2021-09-22 14:01:06 -0400

    Add New-RandomPassword

---
+++ New-RandomPassword.ps1
@@ -0,0 +1,21 @@
+function New-RandomPassword {
+    param (
+        [Parameter(Mandatory)]
+        [int]
+        $WordCount,
+        [Parameter(Mandatory)]
+        [string]
+        $WordSeed
+    )
+
+    $RandomWordList = New-Object 'System.Collections.Generic.List[string]'
+    $Uri = 'https://api.datamuse.com/words?rel_nry={0}&max=100' -f $WordSeed
+    $Result = Invoke-RestMethod -Uri $Uri
+
+    for ($i = 0; $i -lt $WordCount; $i++) {
+        $RandomWord = $Result.Word | Where-Object {$_ -notlike '* *'} | Get-Random
+        $RandomWordList.Add($RandomWord)
+    }
+
+    return (($RandomWordList) -join '!') + '!' + (Get-Random -Minimum 1000 -Maximum 9999)
+}
